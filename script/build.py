f71155da9aca17d05d2d184ca5e8597078b42f31
BulbyVR <26726264+TheDrawingCoder-Gamer@users.noreply.github.com>
2023-12-08 15:03:16 -0500

    Bump SkiaBuild version

--- script/build.py
+++ script/build.py
@@ -6,7 +6,7 @@ def main():
   parser.add_argument('--debug', action='store_true')
   parser.add_argument('--arch', default=build_utils.arch)
   parser.add_argument('--skia-dir')
-  parser.add_argument('--skia-release', default='m116-f44dbc40d8')
+  parser.add_argument('--skia-release', default='m116-d2c211228d')
   parser.add_argument('--cmake-toolchain-file')
   (args, _) = parser.parse_known_args()
 
