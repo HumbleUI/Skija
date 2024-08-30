50907032d2a6f21d9b1764d9d2910faf1d12fba6
Wil Gaboury <wgaboury@gmail.com>
2024-08-30 08:00:38 -0400

    Improve IRI constructors

--- shared/java/svg/SVGIRI.java
+++ shared/java/svg/SVGIRI.java
@@ -9,9 +9,17 @@ public class SVGIRI {
     @ApiStatus.Internal public final SVGIRIType _type;
     @ApiStatus.Internal @Nullable public final String _iri;
 
-    @ApiStatus.Internal
     public SVGIRI(SVGIRIType type, @Nullable String iri) {
         _type = type;
         _iri = iri;
+    }
+
+    @ApiStatus.Internal
+    public SVGIRI(int type, @Nullable String iri) {
+        this(SVGIRIType._values[type], iri);
+    }
+
+    public SVGIRI() {
+        this(SVGIRIType.LOCAL, null);
     }
 }
--- shared/java/svg/SVGPaint.java
+++ shared/java/svg/SVGPaint.java
@@ -2,33 +2,37 @@ package io.github.humbleui.skija.svg;
 
 import lombok.Data;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Nullable;
 
 @Data
 public class SVGPaint {
     @ApiStatus.Internal public final SVGPaintType _type;
 
     // logical union
-    @ApiStatus.Internal @Nullable public final Integer _color;
-    @ApiStatus.Internal @Nullable public final SVGIRI _iri;
+    @ApiStatus.Internal public final int _color;
+    @ApiStatus.Internal public final SVGIRI _iri;
 
     @ApiStatus.Internal
-    public SVGPaint(SVGPaintType type, Integer color, SVGIRI iri) {
+    public SVGPaint(SVGPaintType type, int color, SVGIRI iri) {
         _type = type;
         _color = color;
         _iri = iri;
     }
 
+    @ApiStatus.Internal
+    public SVGPaint(int type, int color, SVGIRI iri) {
+        this(SVGPaintType._values[type], color, iri);
+    }
+
     public SVGPaint() {
-        this(SVGPaintType.NONE, 0x000000, null);
+        this(SVGPaintType.NONE, 0x000000, new SVGIRI());
     }
 
     public SVGPaint(SVGPaintType type) {
-        this(type, 0x000000, null);
+        this(type, 0x000000, new SVGIRI());
     }
 
     public SVGPaint(int color) {
-        this(SVGPaintType.COLOR, color, null);
+        this(SVGPaintType.COLOR, color, new SVGIRI());
     }
 
     public SVGPaint(SVGIRI iri, int fallbackColor) {
