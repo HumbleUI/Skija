08cffea798f3c2cc8ce7a0bc725fcbb62ee9c2d2
Eatgrapes <Eatgrapesem232@qq.com>
2026-01-30 23:25:50 +0800

    fix build.py

--- script/build.py
+++ script/build.py
@@ -6,7 +6,7 @@ def main():
   parser.add_argument('--debug', action='store_true')
   parser.add_argument('--arch', default=build_utils.arch)
   parser.add_argument('--skia-dir')
-  parser.add_argument('--skia-release', default='m143-da51f0d60e')
+  parser.add_argument('--skia-release', default='m143-da51f0d60e-2')
   parser.add_argument('--cmake-toolchain-file')
   (args, _) = parser.parse_known_args()
 
